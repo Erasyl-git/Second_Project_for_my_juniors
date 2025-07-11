3c7566aee50207be06286d70fb5be9fe81c5f064
Erda <lime7517@gmail.com>
2025-07-11 13:50:29 +0500

    fix-models

--- projects/models.py
+++ projects/models.py
@@ -9,12 +9,3 @@ class Project(models.Model):
 
 
 
-class Task(models.Model):
-
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True)
-
-    title = models.CharField(max_length=100)
-    details = models.TextField(max_length=300)
-
-    prority = models.IntegerField(max_length=5)
-    status = models.CharField(max_length=100)
--- task/models.py
+++ task/models.py
@@ -1,13 +1,9 @@
 from django.db import models
 
+from projects.models import Project
 
 
-class Project(models.Model):
 
-    title = models.CharField(max_length=100)
-    description = models.TextField(max_length=300)
-
-    is_active = models.BooleanField(True,False)
 
 
 
