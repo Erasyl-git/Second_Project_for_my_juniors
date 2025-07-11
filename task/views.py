b4696ed23c8a3a455fcc661b88ac3d506d7ae647
Nazar <nazarking03@gmail.com>
2025-07-11 11:46:30 +0500

    migrations and superuser

--- task/views.py
+++ task/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Project
+from .models import Task
+
+
+from serializers import ProjctSerializer
+from serializers import TaskSerializer
