d2d4963ebc6623ab38532e070572e30f44463917
Nazar <nazarking03@gmail.com>
2025-07-11 10:58:15 +0500

    fix commit

---
+++ task/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+
+class Project(models.Model):
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+
+    is_active = models.BooleanField(True,False)
+
+
+
+
+
+
+
+class Task(models.Model):
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True)
+
+    title = models.CharField(max_length=100)
+    details = models.TextField(max_length=300)
+
+    prority = models.IntegerField(max_length=5)
+    status = models.CharField(max_length=100)
