53ef2c0ce0eaa72f163576b35eaa6be8fd2950fb
Erda <lime7517@gmail.com>
2025-07-12 13:16:07 +0500

    fix bug

--- task/models.py
+++ task/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from projects.models import Project
 
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Task(models.Model):
@@ -11,5 +12,5 @@ class Task(models.Model):
     title = models.CharField(max_length=100)
     details = models.TextField(max_length=300)
 
-    priority = models.IntegerField(max_length=5)
+    priority = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     status = models.CharField(max_length=100)
