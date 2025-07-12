187a8adde32f077497ae7747979aa19edb823d9e
Nazar <nazarking03@gmail.com>
2025-07-12 11:03:00 +0500

    views crud fix

--- task/views.py
+++ task/views.py
@@ -1,4 +1,4 @@
-
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -24,15 +24,6 @@ class TaskAPIView(APIView):
          return Response(self.seralizers_class(queryset, many=True).data, status=status.HTTP_200_OK)
     
 
-
-
-
-
-
-
-
-
-
     def post(self,request):
 
 
@@ -48,6 +39,71 @@ class TaskAPIView(APIView):
 
         project.save()
 
+
+    
+    def get_object(self, **kwargs):
+
+        return get_object_or_404(Task, pk=kwargs["pk"])
+    
+
+    
+    def patch(self, request, *args, **kwargs):
+
+
+        queryset = self.get_object(pk=kwargs["pk"])
+
+        task = self.seralizers_class(queryset, data=request.data, partial=True)
+
+        if task.is_valid():
+
+            task.save()
+
+            return Response({"message": "task update"}, status=status.HTTP_200_OK)
+        return Response(task.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+    def put(self, request, *args, **kwargs):
+
+        queryset = self.get_object(pk=kwargs["pk"])
+
+        task = self.seralizers_class(queryset, data=request.data)
+
+        if task.is_valid():
+
+            task.save()
+
+            return Response({"message": "task update put metod"}, status=status.HTTP_200_OK)
+        return Response(task.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+    def delete(self, request, *args, **kwargs):
+
+        queryset = self.get_object(pk=kwargs["pk"])
+
+        id = queryset.id 
+
+        queryset.delete()
+
+        queryset.save()
+
+        return({"message": f"delete task{id}"})
+
+
+
+
+
+
+
+
+
+   
+
+
+    
+    
+       
     
 
 
